78b040a4956d03010cac60b1c02806115180533d
Dominik <dominik.muench@mni.thm.de>
2021-07-06 23:19:21 +0200

    Started working on rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Sprite[] sprites;
     Vector2 movement;
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
@@ -13,6 +14,12 @@ public class PlayerController : MonoBehaviour
     public GameObject bulletPrefab;
     public float bulletForce;
     float angle;
+    SpriteRenderer sR;
+
+    void Start()
+    {
+        sR = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,7 +34,12 @@ public class PlayerController : MonoBehaviour
     {
         rb.MovePosition(rb.position+movement*moveSpeed*Time.fixedDeltaTime);
         Vector2 lookDir = mouse - rb.position;
-        angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f;
+        angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg-90;
+        Debug.Log(angle);
+        if(angle <= -130 && angle > -240) sR.sprite = sprites[0];
+        else if(angle <= -40 && angle > -130) sR.sprite = sprites[1];
+        else if(angle <= 50 && angle > -40) sR.sprite = sprites[2]; // looking top
+        else if(angle <= -240 || angle > 50) sR.sprite = sprites[2];
         gun.rotation = Quaternion.Euler(0,0,angle);
     }
 
