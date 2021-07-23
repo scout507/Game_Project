1f87bb08527987836b94445c721ab55158e56326
Dominik <dominik.muench@mni.thm.de>
2021-07-23 14:59:11 +0200

    spider monster

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -17,6 +17,8 @@ public class MonsterController : MonoBehaviour
     Pathfinding.AIPath pathing;
     Pathfinding.AIDestinationSetter destSetter;
     public Transform dmgPopUp;
+    public Sprite[] sprites;
+    private SpriteRenderer sR;
     
     private bool hasAtkd;
     private float aktTimer;
@@ -33,6 +35,7 @@ public class MonsterController : MonoBehaviour
         pathing = GetComponent<Pathfinding.AIPath>();
         destSetter = GetComponent<Pathfinding.AIDestinationSetter>();
         destSetter.target = player.transform;
+        sR = GetComponentInChildren<SpriteRenderer>();
     }
 
     
@@ -55,6 +58,21 @@ public class MonsterController : MonoBehaviour
             rb.velocity = Vector3.zero;
             Invoke("startMoving", 0.5f);
         }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDir = new Vector2(player.transform.position.x, player.transform.position.y) - rb.position;
+        float facing = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        //down = -112,5 - -67,5 / down-right -67,5 - -22,5 / right -22,5 - 22,5 / top right = 22,5 - 67,5 / top = 67,5 - 112,5 / top-left = 157,5 / left = < 157,5 | > -157,5 / down-left = -112,5 - -157,5
+        if(facing >= -112.5f && facing < -67.5f) sR.sprite = sprites[0]; //down
+        else if(facing >= -67.5f && facing < -22.5f) sR.sprite = sprites[1]; //donw-right
+        else if(facing >= -22.5f && facing < 22.5f) sR.sprite = sprites[2]; //right
+        else if(facing >= 22.5f && facing < 67.5f) sR.sprite = sprites[3]; //top-right
+        else if(facing >= 67.5f && facing < 112.5f) sR.sprite = sprites[4]; //top
+        else if(facing >= 112.5f && facing < 157.5f) sR.sprite = sprites[5]; //top-left
+        else if(facing >= -157.5f && facing < -112.5f) sR.sprite = sprites[7]; //down-left
+        else sR.sprite = sprites[6]; //left
     }
 
     void meleeAtk(){
