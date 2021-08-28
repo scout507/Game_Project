03f18e746c956c0188bc10e89da67542f724b546
Dominik <dominik.muench@mni.thm.de>
2021-08-28 20:10:36 +0200

    clean up

--- GDDProjekt/Assets/Scripts/CameraShake.cs
+++ GDDProjekt/Assets/Scripts/CameraShake.cs
@@ -17,15 +17,17 @@ public class CameraShake : MonoBehaviour
     //      to the chosen game object
     // 5. call the shake function using `StartCoroutine(camShake.Shake(...))`
 
-    public IEnumerable Shake(float duration, float magnitude)
+
+
+    public IEnumerator Shake(float duration, float magnitude)
     {
         Vector3 originalPosition = transform.localPosition;
         float elapsed = 0.0f;
 
         while (elapsed < duration)
         {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
+            float x = originalPosition.x + Random.Range(-1f, 1f) * magnitude;
+            float y = originalPosition.y + Random.Range(-1f, 1f) * magnitude;
 
             transform.localPosition = new Vector3(x, y, originalPosition.z);
 
