a08cdb059dd42a06aa8102ca4652c9f4fc912ea5
socke300 <60921761+socke300@users.noreply.github.com>
2021-09-02 18:36:12 +0200

    Healthbar

---
+++ GDDProjekt/Assets/Sidescroll/Scripts/Defense/Hud.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+    public GameObject rightWarning;
+    public GameObject leftWarning;
+    public WallSidescroll wallRight;
+    public WallSidescroll wallLeft;
+    public GameObject looseText;
+    public AiManager aiManager;
+
+    void Update()
+    {
+        if (wallLeft.life < wallLeft.maxlife / 2) leftWarning.SetActive(true);
+        else leftWarning.SetActive(false);
+
+        if (wallRight.life < wallRight.maxlife / 2) rightWarning.SetActive(true);
+        else rightWarning.SetActive(false);
+
+        if (wallLeft.life <= 0 || wallRight.life <= 0)
+        {
+            leftWarning.SetActive(false);
+            rightWarning.SetActive(false);
+        }
+
+        if (aiManager.gameEndEnemy)
+            looseText.SetActive(true);
+    }
+}
